12b2f97c2b315a90bf91ac5212f3e60e55d4c7e4
Mate9000 <1213286@student.roc-nijmegen.nl>
2024-09-25 16:59:38 +0200

    6 en 7

---
+++ UserInput/src/javaExercise/Opdracht6.java
@@ -0,0 +1,22 @@
+package javaExercise;
+
+public class Opdracht6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 125;
+		int num2 = 24;
+        int result = num + num2;
+		int result2 = num - num2;
+		int result3 = num * num2;
+		int result4 = num / num2;
+		int result5 = num % num2;
+		
+		System.out.println(num+"+"+num2+"="+result);
+		System.out.println(num+"-"+num2+"="+result2);
+		System.out.println(num+"x"+num2+"="+result3);
+		System.out.println(num+"/"+num2+"="+result4);
+		System.out.println(num+"nom"+num2+"="+result5);
+	}
+
+}
---
+++ UserInput/src/javaExercise/Opdracht7.java
@@ -0,0 +1,30 @@
+package javaExercise;
+
+public class Opdracht7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x1=1;
+		int x2=2;
+		int x3=3;
+		int x4=4;
+		int x5=5;
+		int x6=6;
+		int x7=7;
+		int x8=8;
+		int x9=9;
+		int x0=10;
+		
+		int n1=8;
+		int n2=16;
+		int n3=24;
+		int n4=32;
+		int n5=40;
+		int n6=48;
+		int n7=56;
+		int n8=64;
+		int n9=72;
+		int n0=80;
+	}
+
+}

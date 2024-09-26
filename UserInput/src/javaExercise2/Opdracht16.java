6aee42b61472b75f1fec15538f5ff813a06f52fe
Mate9000 <1213286@student.roc-nijmegen.nl>
2024-09-26 22:23:20 +0200

    16 en 17 stuff

--- UserInput/src/javaExercise2/Opdracht16.java
+++ UserInput/src/javaExercise2/Opdracht16.java
@@ -4,7 +4,11 @@ public class Opdracht16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(" +\"\"\"\"\"+\r\n"
+		         + "[| o o |]\r\n"
+		         + " |  ^  |\r\n"
+		         + " | '-' |\r\n"
+		         + " +-----+");
 	}
 
 }

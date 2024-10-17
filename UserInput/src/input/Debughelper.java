ab3fd3c6499e5d38a88970c6671d3d833a2fba84
Mate9000 <1213286@student.roc-nijmegen.nl>
2024-10-17 15:29:36 +0200

    upgrades!!!!

---
+++ UserInput/src/input/Debughelper.java
@@ -0,0 +1,80 @@
+package input;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Debughelper {
+    public static void main(String[] args) {
+        String userInput;
+        int maxAttempts = 10;
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+
+        int x = random.nextInt(7); 
+        int y = random.nextInt(7);
+        int z = random.nextInt(7);
+        int w = random.nextInt(7);
+        
+        String secretCode = "" + x + y + z + w;
+        System.out.println("Secret Code: " + secretCode); 
+
+        for (int i = 1; i <= maxAttempts; i++) {
+            System.out.println("Attempt " + i + ": Enter your 4-digit guess:");
+            userInput = sc.nextLine(); 
+            
+            if (userInput.equals(secretCode)) {
+                System.out.println("Yippieeeee! You win!");
+                break; 
+            } else {
+                
+                int a = Character.getNumericValue(userInput.charAt(0));
+                int b = Character.getNumericValue(userInput.charAt(1));
+                int c = Character.getNumericValue(userInput.charAt(2));
+                int d = Character.getNumericValue(userInput.charAt(3));
+
+
+                if (a == x) {
+                    System.out.print("G");
+                } else if (a == y || a == z || a == w) {
+                    System.out.print("N");
+                } else {
+                    System.out.print("-");
+                }
+
+                if (b == y) {
+                    System.out.print("G");
+                } else if (b == x || b == z || b == w) {
+                    System.out.print("N");
+                } else {
+                    System.out.print("-");
+                }
+
+                if (c == z) {
+                    System.out.print("G");
+                } else if (c == x || c == y || c == w) {
+                    System.out.print("N");
+                } else {
+                    System.out.print("-");
+                }
+
+                if (d == w) {
+                    System.out.print("G");
+                } else if (d == x || d == y || d == z) {
+                    System.out.print("N");
+                } else {
+                    System.out.print("-");
+                }
+
+                System.out.println(); // New line after feedback
+            }
+
+            if (i == maxAttempts) {
+                System.out.println("Womp, Womp! You've used all attempts.");
+            }
+        }
+
+        sc.close(); // Close the scanner
+    }
+}
--- UserInput/src/input/MateCodeBreakerReal.java
+++ UserInput/src/input/MateCodeBreakerReal.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MateCodeBreakerReal {
+	//ik heb dit nodig want anders werkt de hele code niet meer
 	public static void main(String[] args) {
 
 		//ik gebruik dit om later op een andere code te maken die de 4 integers naar 4 digit code in een string kan gezet worden en niet bij elkaar opgeteld
@@ -16,13 +17,20 @@ public class MateCodeBreakerReal {
 
 		Scanner sc = new Scanner(System.in);
 		Random Random = new Random();
-
+		//met deze ints maak ik de 4 digit code apart nummers zodat ik later op de x y z en waparte cijfer maak zodat er later op code controle werkt
 		int x = Random.nextInt(6);
 		int y = Random.nextInt(6);
 		int z = Random.nextInt(6);
 		int w = Random.nextInt(6);
-
+        
+		//door x y z en w zo hier onder zo te zetten heb ik de secret code gemaakt niet met een while loop want ik snap while loop nogsteeds niet
 		String secretCode = ("" + x + y + z + w);
+		
+		String guessStr = sc.next();
+		int a = Character.getNumericValue(guessStr.charAt(0));
+		int b = Character.getNumericValue(guessStr.charAt(0));
+		int c = Character.getNumericValue(guessStr.charAt(0));
+		int d = Character.getNumericValue(guessStr.charAt(0));
 
 		System.out.println(secretCode);
 
@@ -41,6 +49,38 @@ public class MateCodeBreakerReal {
 					
 					if(i < geuss) {
 						System.out.println("Womp, Womp!");
+					
+					if (a == x) {
+					            System.out.print("G");
+					        } else if (a == y || a == z || a == w) {
+					            System.out.print("N");
+					        } else {
+					            System.out.print("-");
+					        }
+					if (b == y) {
+			            System.out.print("G");
+			        } else if (b == x || b == z || b == w) {
+			            System.out.print("N");
+			        } else {
+			            System.out.print("-");
+			        }
+					
+					if (c == z) {
+			            System.out.print("G");
+			        } else if (c == y || c == x || c == w) {
+			            System.out.print("N");
+			        } else {
+			            System.out.print("-");
+			        }
+					
+					if (d == w) {
+			            System.out.print("G");
+			        } else if (a == y || a == z || a == w) {
+			            System.out.print("N");
+			        } else {
+			            System.out.print("-");
+			        }
+						
 			}
 
 		}

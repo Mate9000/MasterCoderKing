c58e46d9b82859449f1d5ce0557839add6b9365c
Mate9000 <1213286@student.roc-nijmegen.nl>
2024-10-18 11:39:08 +0200

    finished product????

--- UserInput/src/input/MateCodeBreakerReal.java
+++ UserInput/src/input/MateCodeBreakerReal.java
@@ -3,32 +3,32 @@ package input;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Debughelper {
+public class MateCodeBreakerReal {
     public static void main(String[] args) {
         String userInput;
         int maxAttempts = 10;
-
+        
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
-
-        int x = random.nextInt(7); 
-        int y = random.nextInt(7);
-        int z = random.nextInt(7);
-        int w = random.nextInt(7);
-        
+        // deze 4 generates een random digit code tussen de 0 en de 5 en ik moeste dit 4 keer doen anders krijg ik gen 4 digit code maar 1 digit code
+        int x = random.nextInt(6); 
+        int y = random.nextInt(6);
+        int z = random.nextInt(6);
+        int w = random.nextInt(6);
+        //hier onder zet ik de 4 aparte digits in een 4 digit rij zodat ze een 4 digit code kunnen worden en ik print de secret code uit om te zien of het werkt of niet
         String secretCode = "" + x + y + z + w;
         System.out.println("Secret Code: " + secretCode); 
-
+        System.out.println("only 4 digit code allowed otherwise game crashes"); 
         for (int i = 1; i <= maxAttempts; i++) {
-            System.out.println("Attempt " + i + ": Enter your 4-digit guess:");
+            System.out.println("Attempt " + i + ": Enter your 4-digit guess from 0 to 5:");
             userInput = sc.nextLine(); 
             
             if (userInput.equals(secretCode)) {
                 System.out.println("Yippieeeee! You win!");
                 break; 
             } else {
-                
+                //hier onder zet ik met deze lijn code de aparte digits in een character zodat ik later op in de coe na kan kijken
                 int a = Character.getNumericValue(userInput.charAt(0));
                 int b = Character.getNumericValue(userInput.charAt(1));
                 int c = Character.getNumericValue(userInput.charAt(2));
@@ -65,16 +65,14 @@ public class Debughelper {
                     System.out.print("N");
                 } else {
                     System.out.print("-");
-                }
-                if(a+b+c+d == a+b+c+d) {
-                	
-                }
+                }   
                 
-                System.out.println(); // New line after feedback
+                
+                System.out.println(); // did maakt zeker dat de nakijk ding niet samen zit met de attempt counter
             }
 
             if (i == maxAttempts) {
-                System.out.println("Womp, Womp! You've used all attempts.");
+                System.out.println("Womp, Womp! You've used all attempts."); //als je max attempts op is dus max attempt 10 gebruit dan stopt alles en je krijgt womp womp 
             }
         }
 

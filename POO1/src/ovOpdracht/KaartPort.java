f33156a77878290843371891d88fae257d2146dd
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-03-19 11:42:48 +0100

    oop

---
+++ POO1/src/ovOpdracht/KaartPort.java
@@ -0,0 +1,13 @@
+package ovOpdracht;
+
+import prison.Prisoner;
+
+public class KaartPort extends Kaart {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Kaart p01 = new Kaart(5, false);
+		
+		print.p01();
+	}
+}

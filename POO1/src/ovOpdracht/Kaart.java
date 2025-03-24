169dcae91abe50f1620f14394634ac84bcc35003
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-03-24 13:25:30 +0100

    a little bit of change
    i removed the unnecsary code and also added one line of code with a bit of comments too

--- POO1/src/ovOpdracht/Kaart.java
+++ POO1/src/ovOpdracht/Kaart.java
@@ -2,42 +2,31 @@ package ovOpdracht;
 
 public class Kaart extends KaartPort {
 
-    private boolean checking;
-    private double saldo;
+	private boolean checking;
+	private double saldo;
 
-    
-    public Kaart(double saldo) {
-        this.saldo = saldo;
-    }
+	// the amount of money i get i will be able to put it in kaartport in kaart p01 on how much i have 
+	public Kaart(double saldo) {
+		this.saldo = saldo;
+	}
 
-
+	// this picks up the card balance to later on check it in kaartport
 	public double getSaldo() {
-        return saldo;
-    }
+		return saldo;
+	}
 
+	//
+	public boolean isChecking() {
+		return checking;
+	}
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
+	public void setChecking(boolean checking) {
+		this.checking = checking;
+	}
 
+	public void displayInfo() {
+		System.out.println("Card Balance: " + saldo);
+		System.out.println("Is Checking Card checked in?: " + checking);
+	}
 
-
-    public boolean isChecking() {
-        return checking;
-    }
-
-
-    public void setChecking(boolean checking) {
-        this.checking = checking;
-    }
-
-
-    public void displayInfo() {
-        System.out.println("Card Balance: " + saldo);
-        System.out.println("Is Checking Card: " + checking);
-    }
-
-    }
-
-
-
+}
--- POO1/src/ovOpdracht/KaartPort.java
+++ POO1/src/ovOpdracht/KaartPort.java
@@ -4,43 +4,38 @@ import java.util.Scanner;
 
 public class KaartPort {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Kaart p01 = new Kaart(5.1);
+		Kaart p01 = new Kaart(5);
+		Scanner scanner = new Scanner(System.in);
 
+		boolean checkingIn = true;
 
-        Scanner scanner = new Scanner(System.in);
+		while (checkingIn) {
+			p01.displayInfo();
+			System.out.println("wil je in checken? (ja/nee)");
+			String userInput = scanner.nextLine();
 
+			if (userInput.equals("ja")) {
+				saldoCheck(p01);
+				checkingIn = false;
+			} else if (userInput.equals("nee")) {
+				System.out.println("je wilde niet inchecken mischien nu well?");
+			} else {
+				System.out.println("ja of nee antwoorden alleen");
+			}
+		}
 
-        boolean checkingIn = true;
+		scanner.close();
+	}
 
+	public static void saldoCheck(Kaart kaart) {
 
-        while (checkingIn) {
+		if (kaart.getSaldo() >= 5.0 && !kaart.isChecking()) {
+			System.out.println("je bent erin gecheckt.");
+		} else {
+			System.out.println("je mist wat geld");
+		}
 
-            System.out.println("wil je in checken? (ja/nee)");
-            String userInput = scanner.nextLine();
-
-
-        if (userInput.equals("ja")) {
-            saldoCheck(p01);
-            checkingIn = false; 
-        } else if (userInput.equals("nee")) {
-            System.out.println("je wilde niet inchecken mischien nu well?");
-        } else {
-            System.out.println("ja of nee antwoorden alleen");
-        }
-    }
-
-    scanner.close();
-    }
-
-    public static void saldoCheck(Kaart kaart) {
-
-    if (kaart.getSaldo() >= 5.0 && !kaart.isChecking()) {
-        System.out.println("je bent erin gecheckt.");
-    } else {
-        System.out.println("je mist wat geld");
-    }
-    
-    }
-    }
+	}
+}
